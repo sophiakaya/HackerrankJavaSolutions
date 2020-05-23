bc0289be6484959f3232e05867410069bb1acfa7
Me <me@Me-MacBook-Air.local>
2020-05-23 18:02:16 +0100

    Hashmaps problems

---
+++ src/com/sophiakaya/hashmaps/TwoStrings.java
@@ -0,0 +1,32 @@
+package com.sophiakaya.hashmaps;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class TwoStrings {
+
+    static String twoStrings(String s1, String s2) {
+
+	Map<String, Long> mapOne = s1.chars().mapToObj(c -> Character.toString((char) c))
+		.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+	Map<String, Long> mapTwo = s2.chars().mapToObj(c -> Character.toString((char) c))
+		.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+	if (mapOne.keySet().stream().anyMatch(v -> mapTwo.containsKey(v))) {
+	    return "YES";
+	}
+
+	return "NO";
+
+    }
+
+    public static void main(String[] args) {
+
+	System.out.println(twoStrings("hello", "world"));
+	System.out.println(twoStrings("hi", "world"));
+
+    }
+
+}
