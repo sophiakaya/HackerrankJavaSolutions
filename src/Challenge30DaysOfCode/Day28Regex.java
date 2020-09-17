caf3b86100c0e971e09e64dff2cbbeb59b348a0b
Sophia KAYA <sophiakaya@Sophias-MacBook-Air.local>
2020-09-17 14:50:00 +0100

    challenge

--- src/Challenge30DaysOfCode/Day28Regex.java
+++ src/Challenge30DaysOfCode/Day28Regex.java
@@ -1,8 +1,53 @@
 package Challenge30DaysOfCode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Sophia Kaya
  */
 public class Day28Regex {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+	int N = scanner.nextInt();
+	scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+	List<String> list = new ArrayList<>();
+	String regexString = "[a-zA-Z\\s]+";
+	String regexEmail = ".+@gmail\\.com$";
+	Pattern patternString = Pattern.compile(regexString);
+	Pattern patternEmail = Pattern.compile(regexEmail);
+
+	Matcher matcherString = null;
+	Matcher matcherEmail = null;
+
+	for (int NItr = 0; NItr < N; NItr++) {
+	    String[] firstNameEmailID = scanner.nextLine().split(" ");
+
+	    String firstName = firstNameEmailID[0];
+
+	    String emailID = firstNameEmailID[1];
+
+	    matcherString = patternString.matcher(firstName);
+	    matcherEmail = patternEmail.matcher(emailID);
+
+	    if (matcherString.find() && matcherEmail.find() && firstName.length() <= 20 && emailID.length() <= 50) {
+		list.add(firstName);
+	    }
+
+	    // System.out.println(firstName);
+	    // System.out.println(emailID);
+
+	}
+
+	list.stream().sorted().forEach(s -> System.out.println(s));
+
+	scanner.close();
+    }
 
 }
--- src/Challenge30DaysOfCode/Day29BitwiseAnd.java
+++ src/Challenge30DaysOfCode/Day29BitwiseAnd.java
@@ -1,8 +1,39 @@
 package Challenge30DaysOfCode;
 
+import java.util.Scanner;
+
 /**
  * @author Sophia Kaya
  */
 public class Day29BitwiseAnd {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+	int t = scanner.nextInt();
+	scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+	for (int tItr = 0; tItr < t; tItr++) {
+	    String[] nk = scanner.nextLine().split(" ");
+
+	    int n = Integer.parseInt(nk[0]);
+
+	    int k = Integer.parseInt(nk[1]);
+
+	    int result = 0;
+	    for (int A = 1; A <= n; A++) {
+		for (int B = A + 1; B <= n; B++) {
+		    int current = A & B;
+		    if (current > result && current < k) {
+			result = current;
+		    }
+		}
+	    }
+	    System.out.println(result);
+
+	}
+
+	scanner.close();
+    }
 
 }
